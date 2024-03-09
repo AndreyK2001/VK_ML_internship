0ef9c9a2926f322d21e77948276b58a1eae971e9
AndreyK2001 <kapelushnikov@gmail.com>
2024-03-09 18:34:39 +0300

    Update get_data.sh

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget "https://drive.google.com/uc?export=download&id=1PCmLbAjAjseei3n_dQVwDqxArs25mIBJ" -O test_df.csv
-wget "https://drive.google.com/uc?export=download&id=1r8iEoCt-qKEzbGrmT414vbv9_rbPlrNW" -O train_df.csv
+wget "https://drive.google.com/uc?export=download&id=1PCmLbAjAjseei3n_dQVwDqxArs25mIBJ" -O /app/data/test_df.csv
+wget "https://drive.google.com/uc?export=download&id=1r8iEoCt-qKEzbGrmT414vbv9_rbPlrNW" -O /app/data/train_df.csv
