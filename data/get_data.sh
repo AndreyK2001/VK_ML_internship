b8d55a1dac159fa7a0af6c09a32922547e141670
AndreyK2001 <kapelushnikov@gmail.com>
2024-03-09 16:59:17 +0300

    Addet Docker

---
+++ data/get_data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget "https://drive.google.com/uc?export=download&id=1PCmLbAjAjseei3n_dQVwDqxArs25mIBJ" -O test_df.csv
+wget "https://drive.google.com/uc?export=download&id=1r8iEoCt-qKEzbGrmT414vbv9_rbPlrNW" -O train_df.csv
